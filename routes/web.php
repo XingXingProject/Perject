83e81104d5277ccd9f9d0aed43da562fb22e17ce
root <22@qq.com>
2018-07-20 19:26:03 +0800

    第一次上传

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//平台路由admin.ele.com
+Route::domain('admin.ele.com')->namespace('Admin')->group(function () {
+    //店铺分类 route('路径'，‘控制器/方法’)
+    Route::get('shop_category/index',"ShopCategoryController@index")->name('shop_category.index');
+    Route::any('shop_category/add',"ShopCategoryController@add")->name('shop_category.add');
+    Route::any('shop_category/edit/{id}',"ShopCategoryController@edit")->name('shop_category.edit');
+    Route::get('shop_category/del/{id}',"ShopCategoryController@del")->name('shop_category.del');
+
+});
+
+
+
+
+//商户shop.ele.com
+Route::domain('shop.ele.com')->namespace('Shop')->group(function () {
+
+    Route::get('user/reg',"UserController@reg");
+    Route::get('user/index',"UserController@index");
+
+});
+
+//用户路由（前端）
